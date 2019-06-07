f91490b8cad2dda6cd36066a0a39cafabb620833
joerlop <jj.erlich155@gmail.com>
2019-06-07 14:52:11 -0500

    Editor customization file uploaded

--- src/components/NewPost/NewEditor.js
+++ src/components/NewPost/NewEditor.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CKEditor from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import "./CustomStyles.scss";
 
 class NewEditor extends Component {
   render() {
