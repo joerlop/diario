fe2a87d7c2adb81e75de196f155e6dc21ca3f8ee
joerlop <jj.erlich155@gmail.com>
2019-06-07 14:33:06 -0500

    Reordered project folders

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.scss'
+import { UserSession } from 'blockstack'
+import NewPost from "./components/NewPost/NewPost";
+import Login from "./components/Login/Login"
+import { Route, Redirect } from "react-router-dom";
+import Home from "./components/Home/Home"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.userSession = new UserSession();
+  }
+
+  componentWillMount() {
+    console.log("hello");
+    const session = this.userSession
+    if(!session.isUserSignedIn() && session.isSignInPending()) {
+      session.handlePendingSignIn()
+      .then((userData) => {
+        if(!userData.username) {
+          throw new Error('This app requires a username.')
+        }
+        window.location = `/newpost`
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/*this.userSession.isUserSignedIn() ? <NewPost /> : <Login />*/}
+        {<Route
+          exact
+          path="/"
+          render={() =>
+            this.userSession.isUserSignedIn() ? (
+              <Redirect to="/newpost" />
+            ) : (
+              <Redirect to="/login" />
+            )
+          }
+        />}
+        <Route
+          exact
+          path="/newpost"
+          render={routeProps => (
+            <NewPost {...routeProps} userSession={this.userSession} />
+          )}
+        />
+        <Route
+          exact
+          path="/home"
+          render={routeProps => (
+            <Home {...routeProps} userSession={this.userSession} />
+          )}
+        />
+        <Route
+          exact
+          path="/login"
+          render={routeProps => (
+            <Login {...routeProps} userSession={this.userSession} />
+          )}
+        />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
