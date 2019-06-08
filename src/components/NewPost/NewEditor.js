bb5ac9ab19ad255582fbcf64dc2978e8a9123e01
joerlop <jj.erlich155@gmail.com>
2019-06-07 21:53:12 -0500

    Added placeholder to Editor

--- src/components/NewPost/NewEditor.js
+++ src/components/NewPost/NewEditor.js
@@ -7,7 +7,7 @@ class NewEditor extends Component {
     return (
         <CKEditor
           editor={ClassicEditor}
-          data="<p>Hello from CKEditor 5!</p>"
+          placeholder="Hello!"
           onInit={editor => {
             // You can store the "editor" and use when it is needed.
             console.log("Editor is ready to use!", editor);
