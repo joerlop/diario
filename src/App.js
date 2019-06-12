71545b6f68b149ad7c331e146a98d17414b0f0cd
joerlop <jj.erlich155@gmail.com>
2019-06-11 21:11:25 -0500

    Added transitions from page to page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
-import './App.scss'
-import { UserSession } from 'blockstack'
+import React, { Component } from "react";
+import "./App.scss";
+import { UserSession } from "blockstack";
 import NewPost from "./components/NewPost/NewPost";
-import Login from "./components/Login/Login"
-import { Route, Redirect } from "react-router-dom";
-import Home from "./components/Home/Home"
+import Login from "./components/Login/Login";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./components/Home/Home";
+import { TransitionGroup, Transition } from "react-transition-group";
+import { play, exit } from './timelines'
 
 class App extends Component {
   constructor() {
@@ -14,57 +16,81 @@ class App extends Component {
 
   componentWillMount() {
     console.log("hello");
-    const session = this.userSession
-    if(!session.isUserSignedIn() && session.isSignInPending()) {
-      session.handlePendingSignIn()
-      .then((userData) => {
-        if(!userData.username) {
-          throw new Error('This app requires a username.')
+    const session = this.userSession;
+    if (!session.isUserSignedIn() && session.isSignInPending()) {
+      session.handlePendingSignIn().then(userData => {
+        if (!userData.username) {
+          throw new Error("This app requires a username.");
         }
-        window.location = `/newpost`
-      })
+        window.location = `/newpost`;
+      });
     }
   }
 
   render() {
     return (
       <div className="App">
-        {/*this.userSession.isUserSignedIn() ? <NewPost /> : <Login />*/}
-        {<Route
-          exact
-          path="/"
-          render={() =>
-            this.userSession.isUserSignedIn() ? (
-              <Redirect to="/newpost" />
-            ) : (
-              <Redirect to="/login" />
-            )
-          }
-        />}
         <Route
-          exact
-          path="/newpost"
-          render={routeProps => (
-            <NewPost {...routeProps} userSession={this.userSession} />
-          )}
-        />
-        <Route
-          exact
-          path="/home"
-          render={routeProps => (
-            <Home {...routeProps} userSession={this.userSession} />
-          )}
-        />
-        <Route
-          exact
-          path="/login"
-          render={routeProps => (
-            <Login {...routeProps} userSession={this.userSession} />
-          )}
+          render={({ location }) => {
+            const { pathname, key } = location;
+
+            return (
+              <TransitionGroup component={null}>
+                <Transition
+                  key={key}
+                  appear={true}
+                  onEnter={(node, appears) => play(pathname, node, appears)}
+                  onExit={(node, appears) => exit(node, appears)}
+                  timeout={{ enter: 750, exit: 150 }}
+                >
+                  <Switch location={location}>
+                    {/*this.userSession.isUserSignedIn() ? <NewPost /> : <Login />*/}
+                    {
+                      <Route
+                        exact
+                        path="/"
+                        render={() =>
+                          this.userSession.isUserSignedIn() ? (
+                            <Redirect to="/newpost" />
+                          ) : (
+                            <Redirect to="/login" />
+                          )
+                        }
+                      />
+                    }
+                    <Route
+                      exact
+                      path="/newpost"
+                      render={routeProps => (
+                        <NewPost
+                          {...routeProps}
+                          userSession={this.userSession}
+                        />
+                      )}
+                    />
+                    <Route
+                      exact
+                      path="/home"
+                      render={routeProps => (
+                        <Home {...routeProps} userSession={this.userSession} />
+                      )}
+                    />
+                    <Route
+                      exact
+                      path="/login"
+                      render={routeProps => (
+                        <Login {...routeProps} userSession={this.userSession} />
+                      )}
+                    />
+                  </Switch>
+                </Transition>
+              </TransitionGroup>
+            );
+          }}
         />
       </div>
     );
   }
 }
 
-export default App
+export default App;
