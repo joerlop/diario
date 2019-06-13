0ffd2bc18dce4362dfae48a7d587b18ae1719e6c
joerlop <jj.erlich155@gmail.com>
2019-06-13 15:39:16 -0500

    Home grid styling

--- src/components/Home/Year.js
+++ src/components/Home/Year.js
@@ -19,10 +19,19 @@ class Year extends React.Component {
   render() {
     return (
       <div className={`YearComponent`}>
+        {this.props.postYears.length == 0 ? (
+          <h2>You have no posts yet!</h2>
+        ) : (
+          <h2>Your posts:</h2>
+        )}
         <h2 onClick={() => this.toggleYearOpen()}>{this.props.year.year}</h2>
         <div className={`MonthList-${this.state.open}`}>
           {this.props.year.months.map(month => (
-            <Month year={this.props.year.year} month={month} posts={this.props.posts}/>
+            <Month
+              year={this.props.year.year}
+              month={month}
+              posts={this.props.posts}
+            />
           ))}
         </div>
       </div>
