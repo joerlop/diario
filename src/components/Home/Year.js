95533003231683cd53091121cd25a92e2070ace3
joerlop <jj.erlich155@gmail.com>
2019-06-16 19:43:16 -0500

    Animating month discovery

--- src/components/Home/Year.js
+++ src/components/Home/Year.js
@@ -7,24 +7,48 @@ class Year extends React.Component {
     this.state = {
       open: false
     };
+    this.monthList = null;
+    this.timeline = new TimelineMax({ paused: true });
   }
+
+  componentDidMount() {
+    this.timeline.from(this.monthList, 0.5, {
+      display: "none",
+      autoAlpha: 0,
+      delay: 0.25,
+      ease: Power1.easeIn
+    });
+  }
+
+  toggleTimeline = event => {
+    event.preventDefault();
+    if (this.state.open) {
+      this.timeline.play();
+    } else {
+      this.timeline.reverse();
+    }
+
+    this.setState({
+      open: !this.state.open
+    })
+  };
 
   render() {
     return (
-        <div ref={div => (this.year = div)} className="Year-Container">
-          <div className="Year">
-            <h2>{this.props.year.year}</h2>
-          </div>
-          <div className={`MonthList-${this.state.open}`}>
-            {this.props.year.months.map(month => (
-              <Month
-                year={this.props.year.year}
-                month={month}
-                posts={this.props.posts}
-              />
-            ))}
-          </div>
+      <div className="Year-Container">
+        <div onClick={e => this.toggleTimeline(e)} className="Year">
+          <h2>{this.props.year.year}</h2>
         </div>
+        <div ref={div => (this.monthList = div)} className={`MonthList`}>
+          {this.props.year.months.map(month => (
+            <Month
+              year={this.props.year.year}
+              month={month}
+              posts={this.props.posts}
+            />
+          ))}
+        </div>
+      </div>
     );
   }
 }
