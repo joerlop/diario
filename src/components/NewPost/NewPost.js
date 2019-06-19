2ef28ed42e79a2ba800fc76ccc1651930882904d
joerlop <jj.erlich155@gmail.com>
2019-06-19 18:28:43 -0500

    Debugging

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -73,33 +73,34 @@ class NewPost extends React.Component {
 
     console.log(!this.props.gettingPostsError && !this.props.gettingYearsError)
 
-    if (!this.props.gettingPostsError && !this.props.gettingYearsError) {
-      this.timeline
-        .from(this.title, 0.4, {
-          autoAlpha: 0,
-          delay: 0.3,
-          ease: Power1.easeIn
-        })
-        .from(this.editor, 0.4, {
-          autoAlpha: 0,
-          y: 25,
-          ease: Power1.easeInOut
-        })
-        .from(this.feeling, 0.3, {
-          autoAlpha: 0,
-          ease: Power1.easeIn
-        })
-        .from(this.save, 0.3, {
-          autoAlpha: 0,
-          ease: Power1.easeIn
-        })
-        .from(this.date, 0.3, {
-          delay: 0.3,
-          autoAlpha: 0,
-          ease: Power1.easeIn
-        });
-    }
+    /*if (!this.props.gettingPostsError && !this.props.gettingYearsError) {
 
+    }*/
+
+    this.timeline
+    .from(this.title, 0.4, {
+      autoAlpha: 0,
+      delay: 0.3,
+      ease: Power1.easeIn
+    })
+    .from(this.editor, 0.4, {
+      autoAlpha: 0,
+      y: 25,
+      ease: Power1.easeInOut
+    })
+    .from(this.feeling, 0.3, {
+      autoAlpha: 0,
+      ease: Power1.easeIn
+    })
+    .from(this.save, 0.3, {
+      autoAlpha: 0,
+      ease: Power1.easeIn
+    })
+    .from(this.date, 0.3, {
+      delay: 0.3,
+      autoAlpha: 0,
+      ease: Power1.easeIn
+    });
     this.timeline.play();
 
     localStorage.removeItem("signingIn");
