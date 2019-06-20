3866997f901cb53073e0a70bc3321e461544bdf4
joerlop <jj.erlich155@gmail.com>
2019-06-20 09:51:35 -0500

    Home Error Handling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,6 @@ import { Power1 } from "gsap/src/uncompressed/TweenMax";
 import TimelineMax from "gsap/src/uncompressed/TimelineMax";
 import Loader from "react-loader-spinner";
 
-
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -19,32 +18,31 @@ class Home extends React.Component {
   }
 
   componentDidMount() {
-    this.props.getPosts(this.userSession);
-    this.props.getYears(this.userSession).then(() => {
-      this.timeline.from(this.yearList, 0.5, {
-        display: "none",
-        autoAlpha: 0,
-        delay: 0.25,
-        ease: Power1.easeIn
+    this.props.getPosts(this.userSession).then(() => {
+      this.props.getYears(this.userSession).then(() => {
+        this.timeline.from(this.yearList, 0.5, {
+          display: "none",
+          autoAlpha: 0,
+          delay: 0.25,
+          ease: Power1.easeIn
+        });
+        this.timeline.play();
       });
-
-      this.timeline.play();
-      console.log(this.props.postYears)
     });
   }
 
   reverseTimeline = event => {
     event.preventDefault();
     this.timeline.reverse();
-    const timelineDuration = this.timeline.duration()*1000;
+    const timelineDuration = this.timeline.duration() * 1000;
     console.log(timelineDuration);
     setTimeout(() => {
       this.props.history.push("/newpost");
     }, timelineDuration);
-  }
+  };
 
   render() {
-    console.log(this.props.postYears)
+    console.log(this.props.postYears);
     return (
       <div className="Home-container">
         <div className="Navigation">
@@ -52,24 +50,33 @@ class Home extends React.Component {
             <p>home</p>
           </NavLink>
           <h2>diario</h2>
-          <NavLink onClick={e => this.reverseTimeline(e)} className="newpost" to={`/newpost`}>
+          <NavLink
+            onClick={e => this.reverseTimeline(e)}
+            className="newpost"
+            to={`/newpost`}
+          >
             <p>new post</p>
           </NavLink>
         </div>
         <div ref={div => (this.yearList = div)} className={`YearList`}>
-        {this.props.gettingYears ? (
-              <Loader type="ThreeDots" color="#000000" height="15" width="30" />
-            ) : (
-              this.props.postYears.length == 0 ? (
-                <div className="noposts">
-                  <h2>You have no posts yet!</h2>
-                </div>
-              ) : (
-                this.props.postYears.map(year => (
-                  <Year year={year} posts={this.props.posts} />
-                ))
-              )
-            )}
+          {this.props.gettingYears || this.props.gettingPosts ? (
+            <Loader type="ThreeDots" color="#000000" height="15" width="30" />
+          ) : this.props.gettingPostsError || this.props.gettingYearsError ? (
+            <div className="Error">
+              <p>
+                Oops! We had a problem retrieving your info. Please try again
+                later.
+              </p>
+            </div>
+          ) : this.props.postYears.length == 0 ? (
+            <div className="noposts">
+              <h2>You have no posts yet!</h2>
+            </div>
+          ) : (
+            this.props.postYears.map(year => (
+              <Year year={year} posts={this.props.posts} />
+            ))
+          )}
         </div>
       </div>
     );
@@ -79,7 +86,10 @@ class Home extends React.Component {
 const mapStateToProps = state => ({
   posts: state.postsReducer.posts,
   postYears: state.postsReducer.postYears,
-  gettingYears: state.postsReducer.gettingYears
+  gettingYears: state.postsReducer.gettingYears,
+  gettingPostsError: state.postsReducer.gettingPostsError,
+  gettingYearsError: state.postsReducer.gettingYearsError,
+  gettingPosts: state.postsReducer.gettingPosts
 });
 
 export default connect(
