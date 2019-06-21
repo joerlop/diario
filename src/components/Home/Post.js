9228aa645fb2ac66bb9d7c7d89f2550c6279a451
joerlop <jj.erlich155@gmail.com>
2019-06-20 21:38:10 -0500

    Emojis, date and title showing in Home

--- src/components/Home/Post.js
+++ src/components/Home/Post.js
@@ -1,23 +1,54 @@
 import React from "react";
+import Feelings from "./Feelings";
 
 class Post extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
     this.state = {
       open: false
     };
   }
 
+  handleToggle(e) {
+    e.preventDefault();
+    const prevHeight = this.state.height || 0;
+    this.setState(
+      {
+        open: !this.state.open,
+        height: this.refs.inner.clientHeight
+      },
+      () => {
+        console.log(prevHeight);
+        this.state.open
+          ? this.props.updateHeightYear(this.state.height)
+          : this.props.updateHeightYear(-prevHeight);
+      }
+    );
+  }
+
   render() {
+    const { open, height } = this.state;
+    const currentHeight = open ? height : 0;
+
     return (
       <div className="Post">
-        <div
-          className={`Content`}
-          dangerouslySetInnerHTML={{ __html: this.props.post.data }}
-        />
-        <div className="Characteristics">
-          <p>{this.props.post.day}</p>
+        <div className="title-section" onClick={e => this.handleToggle(e)}>
+          <div className="title">{this.props.post.title}</div>
+          <div className="characteristics">
+            <div className="feelings-container">
+              <Feelings feelings={this.props.post.feelings} />
+            </div>
+            <div className="day">
+              <p>{this.props.post.day}</p>
+            </div>
+          </div>
+        </div>
+        <div className="Content" style={{ height: currentHeight + "px" }}>
+          <div
+            className={`Content-inner`}
+            ref="inner"
+            dangerouslySetInnerHTML={{ __html: this.props.post.data }}
+          />
         </div>
       </div>
     );
