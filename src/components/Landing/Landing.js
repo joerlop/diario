c34668a29f6a15ae30564b9a469c8fe42281b312
joerlop <jj.erlich155@gmail.com>
2019-06-23 17:07:58 -0500

    Landing page content structures

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -16,6 +16,23 @@ class Marketing extends React.Component {
             <p>Sign Up/In</p>
           </Link>
         </nav>
+        <div className="landing-content">
+          <div className="title">
+            <h2>A new kind of diary.</h2>
+          </div>
+          <div className="howitworks">
+            <div className="attr">
+              <h3>Private</h3>
+            </div>
+            <div className="attr">
+              <h3>Simple</h3>
+            </div>
+            <div className="attr">
+              <h3>Fun</h3>
+            </div>
+          </div>
+          <button className="btn">Let's go!</button>
+        </div>
       </div>
     );
   }
